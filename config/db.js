0696997c4de18d35945073d8dd046fbec3e70b0a
Rana <ranaeng500238@gmail.com>
2026-07-03 16:44:56 +0300

    install packages & write index code & db connection code

---
+++ config/db.js
@@ -0,0 +1,24 @@
+import dotenv from "dotenv";
+dotenv.config();
+import mysql from "mysql2/promise";
+
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+try {
+  const connection = await db.getConnection();
+  console.log("✅ MySQL Connected Successfully");
+  connection.release();
+} catch (err) {
+  console.error("❌ Database Connection Failed:", err.message);
+}
+
+export default db;
