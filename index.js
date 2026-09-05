1f71d0d923778bee82435301d3ea97600b250be1
Omnia Abdelnasser <omniaabdelnasser067@example.com>
2026-09-05 21:54:04 +0300

    updates

--- index.js
+++ index.js
@@ -80,9 +80,9 @@ app.use((req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.use(errorHandler);
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+  console.log(`🚀 Server is running on http://${PORT}`);
 });
