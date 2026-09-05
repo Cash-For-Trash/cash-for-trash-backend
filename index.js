05165f0f1ee7857ee943117f7d62d81c1c3aade4
Abdallah-Alqiran <abdallahalqiran765@gmail.com>
2026-09-05 22:09:40 +0300

    refactor index.js

--- index.js
+++ index.js
@@ -80,9 +80,9 @@ app.use((req, res) => {
   });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.use(errorHandler);
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server is running on http://${PORT}`);
+  console.log(`🚀 Server is running on http://localhost:${PORT}`);
 });
